5566e2f6b59934477d05ccb13ce55b56e0404a67
barcelos32 <37002348+barcelos32@users.noreply.github.com>
2018-05-04 16:17:27 -0300

    Update config.js

--- projeto/config.js
+++ projeto/config.js
@@ -3,9 +3,12 @@ var config = {};
 config.debug = process.env.DEBUG || false;
 
 config.mqtt  = {};
-config.mqtt.namespace = process.env.MQTT_NAMESPACE || '#';
-config.mqtt.hostname  = process.env.MQTT_HOSTNAME  || 'localhost';
+config.mqtt.namespace = process.env.MQTT_NAMESPACE || '+/sensores/+/up';
+config.mqtt.hostname  = process.env.MQTT_HOSTNAME  || 'mqtts://35.198.8.16';
 config.mqtt.port      = process.env.MQTT_PORT      || 1883;
+config.mqtt.user      = process.env.MQTT_USER      || 'barcelos32';
+config.mqtt.password  = process.env.MQTT_PASSWORD  || 'sonyk800';
+config.mqtt.cafile    = process.env.MQTT_CAFILE    || 'mqtt-ca.pem';
 
 config.mongodb = {};
 config.mongodb.hostname   = process.env.MONGODB_HOSTNAME   || 'localhost';
@@ -14,3 +17,4 @@ config.mongodb.database   = process.env.MONGODB_DATABASE   || 'mqtt';
 config.mongodb.collection = process.env.MONGODB_COLLECTION || 'message';
 
 module.exports = config;
+
