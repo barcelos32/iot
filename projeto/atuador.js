fcef64823ffe444e414daae31b76b8046e2b7ca7
barcelos32 <37002348+barcelos32@users.noreply.github.com>
2018-05-05 13:56:38 -0300

    Add files via upload

--- projeto/atuador.js
+++ projeto/atuador.js
@@ -7,11 +7,14 @@
 var express = require('express'); 
 var cors = require('cors'); 
 var bodyParser = require('body-parser'); 
-var atuador = require('./models/atuador_bd_connect'); // Modelos definidos
+var Atuador = require('./models/atuador_db_connect'); // Modelos definidos
 var mongoose = require('mongoose');
+var mqtt = require('mqtt');
 
-var url = "mongodb://localhost:27017/atuador";
-mongoose.connect(url);
+require('mongoose-middleware').initialize(mongoose);
+
+mongoose.connect("mongodb://localhost:27017/sensores");
+var client = mqtt.connect('tcp://localhost'); //inicia o mqtt
 
 var app = express(); // Cria o app com Express
 var router = express.Router();
@@ -21,6 +24,37 @@ app.use(bodyParser.urlencoded({
 	extended : true
 })); 
 app.use(bodyParser.json()); // configurações do body parser
+
+client.on('connect', function () {
+   	 client.subscribe('sensores'); //conecta e assina o tópico MQTT
+});
+
+
+client.on('message', function (topic, message) { //aguarda mensagem do tópico assinado MQTT 
+	  console.log(topic.toString());
+	  console.log(message.toString());
+	  var payload       = message.toString();
+	  var message_topic = topic.toString();
+	  
+	  var atuador = new Atuador();
+
+	  var d = new Date();
+	 
+	  atuador.time = d.getFullYear() + "-"
+		+ ("00" + (d.getMonth() + 1)).slice(-2) + "-"
+		+ ("00" + (d.getDate())).slice(-2) + " "
+		+ d.toLocaleTimeString();
+	 
+	  atuador.valor = payload;
+
+		atuador.save(function(error) { // insere no db
+			if (error)
+				console.log(error);
+
+			console.log("Inserido com Sucesso!")
+		});
+	
+});
 
 /* Rota para acompanhar as requisições */
 router.use(function(req, res, next) {
@@ -37,7 +71,15 @@ router.get('/', function(req, res) {
 
 //GET /atuador
 router.route('/atuador').get(function(req, res) {
-	atuador.find(function(err, atuador) {
+	var limit = parseInt(req.query._limit) || 20;
+	var valor = req.query.valor || {$gte: 0};
+	var sort = parseInt(req.query._sort) || -1;
+	Atuador.
+	find().
+	where({ valor: valor }).
+	limit(limit).
+	sort({ _id: sort })
+	.exec(function(err, atuador) {
 		if (err)
 			res.send(err);
 
@@ -46,15 +88,14 @@ router.route('/atuador').get(function(req, res) {
 	console.log('GET /atuador');
 });
 
-//GET /atuador/:id
-router.route('/atuador/:id').get(function(req, res) {
-	atuador.findById(req.params.id, function(error, atuador) {
-		if(error)
-			res.send(error);
+router.route('/atuador/q').get(function(req, res) {
+	Atuador.apiQuery(req.query).exec(function(err, atuador) {
+		if (err)
+			res.send(err);
 
 		res.json(atuador);
 	});
-	console.log('GET /atuador/:id');
+	console.log('GET /atuador/q');
 });
 
 //GET /atuador/recente
@@ -74,27 +115,62 @@ router.route('/atuador/recente').get(function(req, res) {
 	console.log('GET /atuador/recente');
 });
 
+//GET /atuador/elevada
+router.route('/atuador/elevada').get(function(req, res) {
+	var limit = 10;
+	var valor = {$gte: 30};
+	var sort =  -1;
+	
+    Atuador.
+	find().
+	where({ valor: valor }).
+	limit(limit).
+	sort({ _id: sort })
+	.exec(function(err, atuador) {
+		if (err)
+			res.send(err);
+
+		res.json(atuador);
+	});
+    console.log('GET /atuador/elevada');
+});
+
+
+//GET /atuador/:id
+router.route('/atuador/:id').get(function(req, res) {
+	Atuador.findById(req.params.id, function(error, atuador) {
+		if(error)
+			res.send(error);
+
+		res.json(atuador);
+	});
+	console.log('GET /atuador/:id');
+});
+
 /* POST /atuador {time:"..",valor:"..."} */
 router.route('/atuador').post(function(req, res) {
-	var atuador = new atuador();
+	var atuador = new Atuador();
 
 	atuador.time = req.body.time;
 	atuador.valor = req.body.valor;
 
+	client.publish('sensores',  atuador.valor); //MQTT: publica o valor da atuador no Tópico
+	
 	atuador.save(function(error) {
 		if (error)
 			res.send(error);
 
 		res.json({
-			message : 'atuador atualizado!'
+			message : 'atuador inserida e publicada!'
 		});
 	});
+		
 	console.log('POST /atuador');
 });
 
 //PUT /atuador/:id {time:"..",valor:"..."}
 router.route('/atuador/:id').put(function(req, res) {
-	atuador.findById(req.params.id, function(error, atuador) {
+	Atuador.findById(req.params.id, function(error, atuador) {
 		if(error)
 			res.send(error);
 
@@ -104,7 +180,7 @@ router.route('/atuador/:id').put(function(req, res) {
 		atuador.save(function(error) {
 			if(error)
 				res.send(error);
-			res.json({ message: 'atuador Atualizado!' });
+			res.json({ message: 'Atuador Atualizado!' });
 		});
 	});
 	console.log('PUT /atuador/:id');
@@ -112,12 +188,12 @@ router.route('/atuador/:id').put(function(req, res) {
 
 //DELETE /atuador/:id
 router.route('/atuador/:id').delete(function(req, res) {
-	atuador.remove({
+	Atuador.remove({
 		_id: req.params.id
 	}, function(error) {
 		if(error)
 			res.send(error);
-		res.json({ message: 'atuador excluída com Sucesso! '});
+		res.json({ message: 'Atuador excluída com Sucesso! '});
 	});
 	console.log('DELETE /atuador/:id');
 });
